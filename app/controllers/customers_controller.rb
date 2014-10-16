807a12b7961c1437735cca13ef81ebafb225a8ff
fushang318 <fushang318@gmail.com>
2014-10-16 18:21:45 +0800

    fix

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -7,9 +7,15 @@ class CustomersController < ApplicationController
         :storepass         => customer.storepass,
         :keypass           => customer.keypass,
         :http_site         => customer.http_site,
-        :newest_version    => customer.versions.first.version
+        :newest_version    => newest_version(customer)
       }
     end
     render :json => result
+  end
+
+  private 
+  def newest_version(customer)
+    return "0.0.0" if customer.versions.count == 0
+    customer.versions.first.version
   end
 end
